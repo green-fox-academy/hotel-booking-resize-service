fde5c28d9491742d463c869a0af6c1cb76b4da51
Zandraa Perjesi <zandraa.perjesi@gmail.com>
2017-06-08 14:47:50 +0200

    add repository service tests

--- src/main/java/com/greenfox/malachit/repository/HealthCheckRepository.java
+++ src/main/java/com/greenfox/malachit/repository/HealthCheckRepository.java
@@ -2,8 +2,9 @@ package com.greenfox.malachit.repository;
 
 import com.greenfox.malachit.model.HealthCheck;
 import org.springframework.data.repository.CrudRepository;
-import java.util.List;
+
+import java.util.ArrayList;
 
 public interface HealthCheckRepository extends CrudRepository<HealthCheck, Long>{
-  public List<HealthCheck> findAllByOrderById();
+  public ArrayList<HealthCheck> findAllByOrderById();
 }
--- src/test/java/com/greenfox/malachit/HearthbeatTest.java
+++ src/test/java/com/greenfox/malachit/HearthbeatTest.java
@@ -1,48 +1,41 @@
 package com.greenfox.malachit;
 
+import com.greenfox.malachit.model.HealthCheck;
 import com.greenfox.malachit.repository.HealthCheckRepository;
+import com.greenfox.malachit.service.HearthBeatService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.BDDMockito;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.mockito.Mockito;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
-import java.nio.charset.Charset;
-import static org.hamcrest.core.Is.is;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
+import java.util.ArrayList;
+import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
 public class HearthbeatTest {
 
-  private MediaType contentType = new MediaType(MediaType.APPLICATION_JSON.getType(),
-          MediaType.APPLICATION_JSON.getSubtype(),
-          Charset.forName("utf8"));
-
-  @Autowired
-  private MockMvc mockMvc;
-
-  @MockBean
-  private HealthCheckRepository mealRepository;
+  @Test
+  public void getHearthbeatError() {
+    ArrayList<HealthCheck> toReturn = new ArrayList<>();
+    HealthCheckRepository mockedHealthCheckRepository = Mockito.mock(HealthCheckRepository.class);
+    Mockito.when(mockedHealthCheckRepository.findAllByOrderById())
+            .thenReturn(toReturn);
+    HearthBeatService hearthBeatServiceUnderTest = new HearthBeatService(mockedHealthCheckRepository);
+    assertEquals(hearthBeatServiceUnderTest.healthStatus().getDatabase(), "error");
+  }
 
   @Test
-  public void getHearthbeat() throws Exception {
-
-    mockMvc.perform(get("/hearthbeat")
-            .contentType(MediaType.APPLICATION_JSON))
-            .andExpect(status().isOk())
-            .andExpect(content().contentType(contentType))
-            .andExpect(jsonPath("$.status", is("ok")))
-            .andExpect(jsonPath("$.database", is("ok")));
+  public void getHearthbeatOk() {
+    ArrayList<HealthCheck> toReturn = new ArrayList<>();
+    toReturn.add(new HealthCheck(true));
+    HealthCheckRepository mockedHealthCheckRepository = Mockito.mock(HealthCheckRepository.class);
+    Mockito.when(mockedHealthCheckRepository.findAllByOrderById())
+            .thenReturn(toReturn);
+    HearthBeatService hearthBeatServiceUnderTest = new HearthBeatService(mockedHealthCheckRepository);
+    assertEquals(hearthBeatServiceUnderTest.healthStatus().getDatabase(), "ok");
   }
 
 }
