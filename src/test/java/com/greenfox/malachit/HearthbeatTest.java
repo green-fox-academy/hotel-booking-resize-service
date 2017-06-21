20231c8180e5e3830d94b94c7d99da64eced1c72
Zandraa Perjesi <zandraa.perjesi@gmail.com>
2017-06-21 11:25:31 +0200

    modify tests to fit new endpoint response format

--- src/test/java/com/greenfox/malachit/HearthbeatTest.java
+++ src/test/java/com/greenfox/malachit/HearthbeatTest.java
@@ -3,6 +3,7 @@ package com.greenfox.malachit;
 import com.greenfox.malachit.model.HealthCheck;
 import com.greenfox.malachit.repository.HealthCheckRepository;
 import com.greenfox.malachit.service.HearthBeatService;
+import com.greenfox.malachit.service.MessageQueueService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
@@ -18,23 +19,25 @@ import static org.junit.Assert.*;
 public class HearthbeatTest {
 
   @Test
-  public void getHearthbeatError() {
+  public void getHearthbeatError() throws Exception {
     ArrayList<HealthCheck> toReturn = new ArrayList<>();
     HealthCheckRepository mockedHealthCheckRepository = Mockito.mock(HealthCheckRepository.class);
+    MessageQueueService mockedMessageQueueService = Mockito.mock(MessageQueueService.class);
     Mockito.when(mockedHealthCheckRepository.findAllByOrderById())
             .thenReturn(toReturn);
-    HearthBeatService hearthBeatServiceUnderTest = new HearthBeatService(mockedHealthCheckRepository);
+    HearthBeatService hearthBeatServiceUnderTest = new HearthBeatService(mockedHealthCheckRepository, mockedMessageQueueService);
     assertEquals(hearthBeatServiceUnderTest.healthStatus().getDatabase(), "error");
   }
 
   @Test
-  public void getHearthbeatOk() {
+  public void getHearthbeatOk() throws Exception {
     ArrayList<HealthCheck> toReturn = new ArrayList<>();
     toReturn.add(new HealthCheck(1));
     HealthCheckRepository mockedHealthCheckRepository = Mockito.mock(HealthCheckRepository.class);
+    MessageQueueService mockedMessageQueueService = Mockito.mock(MessageQueueService.class);
     Mockito.when(mockedHealthCheckRepository.findAllByOrderById())
             .thenReturn(toReturn);
-    HearthBeatService hearthBeatServiceUnderTest = new HearthBeatService(mockedHealthCheckRepository);
+    HearthBeatService hearthBeatServiceUnderTest = new HearthBeatService(mockedHealthCheckRepository, mockedMessageQueueService);
     assertEquals(hearthBeatServiceUnderTest.healthStatus().getDatabase(), "ok");
   }
 
