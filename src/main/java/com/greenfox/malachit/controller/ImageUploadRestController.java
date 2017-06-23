6f0dc58eed978b7199c0349ee02abf5cefa05946
Zandraa Perjesi <zandraa.perjesi@gmail.com>
2017-06-23 11:13:20 +0200

    refactor controller to work with imageservice

--- src/main/java/com/greenfox/malachit/controller/ImageUploadRestController.java
+++ src/main/java/com/greenfox/malachit/controller/ImageUploadRestController.java
@@ -3,6 +3,8 @@ package com.greenfox.malachit.controller;
 import com.greenfox.malachit.model.FileData;
 import com.greenfox.malachit.model.ImageData;
 import com.greenfox.malachit.model.ImageResponse;
+import com.greenfox.malachit.service.ImageService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,16 +13,15 @@ import org.springframework.web.multipart.MultipartFile;
 @RestController
 public class ImageUploadRestController {
 
-//  @Autowired ImageDataRepository imageDataReposytory;
+  private ImageService imageService;
+
+  @Autowired
+  public ImageUploadRestController(ImageService imageService) {
+    this.imageService = imageService;
+  }
 
   @PostMapping(value="/images", headers="content-type=multipart/*")
   public ImageResponse getFile(@RequestParam("fileName") MultipartFile file){
-    ImageResponse imageResponse = new ImageResponse();
-    String fileType = file.getContentType();
-    ImageData imageData = new ImageData(file.getName());
-    FileData fileData = new FileData(file.getContentType(), imageData);
-//    imageDataReposytory.save(imageData);
-    imageResponse.setData(fileData);
-    return imageResponse;
+    return  imageService.createResponse(file);
   }
 }
