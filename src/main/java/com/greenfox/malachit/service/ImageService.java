4d934367350ab9f5338c4ac866e823d5d42f742c
Zandraa Perjesi <zandraa.perjesi@gmail.com>
2017-06-23 10:28:33 +0200

    commit for merge

---
+++ src/main/java/com/greenfox/malachit/service/ImageService.java
@@ -0,0 +1,13 @@
+package com.greenfox.malachit.service;
+
+import com.greenfox.malachit.model.ImageResponse;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
+public class ImageService {
+
+  public ImageResponse createResponse(MultipartFile file) {
+
+  }
+}
