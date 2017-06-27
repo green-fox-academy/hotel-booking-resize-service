3c5dceaf8f476a93e4e06b471ef8a9b481e8e862
Zandraa Perjesi <zandraa.perjesi@gmail.com>
2017-06-27 16:03:42 +0200

    add thumbnail response mopdel

---
+++ src/main/java/com/greenfox/malachit/model/SelfUrl.java
@@ -0,0 +1,14 @@
+package com.greenfox.malachit.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class SelfUrl {
+  String self;
+}
---
+++ src/main/java/com/greenfox/malachit/model/ThumbnailResponse.java
@@ -0,0 +1,15 @@
+package com.greenfox.malachit.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ThumbnailResponse {
+  SelfUrl links;
+  FileData data;
+}
