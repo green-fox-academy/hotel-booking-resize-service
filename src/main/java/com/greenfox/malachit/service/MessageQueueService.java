638ae64ca5650a53cbac99980913b013d58a2bf1
Zsolt Nagy <nagyza@gmail.com>
2017-06-27 09:25:39 +0200

    Refactor: create connection handling methods

--- src/main/java/com/greenfox/malachit/service/MessageQueueService.java
+++ src/main/java/com/greenfox/malachit/service/MessageQueueService.java
@@ -9,29 +9,21 @@ import java.io.IOException;
 public class MessageQueueService {
 
   private final static String QUEUE_NAME = "hello";
+  private ConnectionFactory factory ;
+  private Connection connection;
+  private Channel channel;
 
   public void sendMessage() throws Exception{
-
-    ConnectionFactory factory = new ConnectionFactory();
-    factory.setUri(System.getenv("RABBIT"));
-    Connection connection = factory.newConnection();
-    Channel channel = connection.createChannel();
+    initMQ();
     channel.queueDeclare(QUEUE_NAME, false, false, false, null);
     String message = "ok";
     channel.basicPublish("", QUEUE_NAME, MessageProperties.PERSISTENT_TEXT_PLAIN,"message".getBytes());
-
-    channel.close();
-    connection.close();
+    closeMQ();
   }
 
   public void receiveMessage() throws Exception{
-
-    ConnectionFactory factory = new ConnectionFactory();
-    factory.setUri(System.getenv("RABBIT"));
-    Connection connection = factory.newConnection();
-    Channel channel = connection.createChannel();
+    initMQ();
     channel.queueDeclare(QUEUE_NAME, false, false, false, null);
-
     Consumer consumer = new DefaultConsumer(channel) {
       @Override
       public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body)
@@ -40,30 +32,31 @@ public class MessageQueueService {
       }
     };
     channel.basicConsume(QUEUE_NAME, true, consumer);
-
-    channel.close();
-    connection.close();
+    closeMQ();
   }
 
   public int messagesInQueue() throws Exception{
-    ConnectionFactory factory = new ConnectionFactory();
-    factory.setUri(System.getenv("RABBIT"));
-    Connection connection = factory.newConnection();
-    Channel channel = connection.createChannel();
-
+    initMQ();
     AMQP.Queue.DeclareOk dok = channel.queueDeclare(QUEUE_NAME, false, false, false, null);
     return dok.getMessageCount();
   }
 
   public void dispatch(String inputMessage) throws Exception{
-    ConnectionFactory factory = new ConnectionFactory();
-    factory.setUri(System.getenv("RABBIT"));
-    Connection connection = factory.newConnection();
-    Channel channel = connection.createChannel();
+    initMQ();
     channel.queueDeclare(System.getenv("SEND_EVENT_QUEUE_NAME"), false, false, false, null);
     String message = new EventCreator(inputMessage).createEventString();
     channel.basicPublish("", QUEUE_NAME, MessageProperties.PERSISTENT_TEXT_PLAIN,"message".getBytes());
+    closeMQ();
+  }
 
+  private void initMQ() throws Exception {
+    factory = new ConnectionFactory();
+    factory.setUri(System.getenv("RABBIT"));
+    connection = factory.newConnection();
+    channel = connection.createChannel();
+  }
+
+  private void closeMQ() throws Exception {
     channel.close();
     connection.close();
   }
