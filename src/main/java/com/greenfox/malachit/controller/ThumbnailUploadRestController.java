4952de8481cdf3343ff5531b324b1bd22e9134e7
btekse <btekse@gmail.com>
2017-06-28 11:36:29 +0200

    add thumbnail upload restcontroller

---
+++ src/main/java/com/greenfox/malachit/controller/ThumbnailUploadRestController.java
@@ -0,0 +1,25 @@
+package com.greenfox.malachit.controller;
+
+import com.greenfox.malachit.model.ImageResponse;
+import com.greenfox.malachit.service.ImageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+public class ThumbnailUploadRestController {
+
+  private ImageService imageService;
+
+  @Autowired
+  public ThumbnailUploadRestController(ImageService imageService) {
+    this.imageService = imageService;
+  }
+
+  @PostMapping(value = "/hotels/{imageId}/thumbnail")
+  @ResponseBody
+  public ImageResponse getfile(@RequestBody ImageResponse imageResponse, @PathVariable long imageId) throws Exception {
+    imageResponse.getData().getAttributes();
+    return imageResponse;
+  }
+}
