b0e9e827858faee2575660085d47fac285374922
Zandraa Perjesi <zandraa.perjesi@gmail.com>
2017-06-30 13:16:21 +0200

    refactor custom exception

--- src/main/java/com/greenfox/malachit/controller/ThumbnailUploadRestController.java
+++ src/main/java/com/greenfox/malachit/controller/ThumbnailUploadRestController.java
@@ -6,9 +6,6 @@ import com.greenfox.malachit.service.ThumbnailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 @RestController
