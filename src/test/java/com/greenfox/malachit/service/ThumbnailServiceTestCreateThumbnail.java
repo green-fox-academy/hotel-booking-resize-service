20eadb98228c04f23124a50021a61b814467dad8
Zsolt Nagy <nagyza@gmail.com>
2017-07-04 16:58:40 +0200

    Create test of method which called flow of get single thumbnail endpoint

--- src/test/java/com/greenfox/malachit/service/ThumbnailServiceTestCreateThumbnail.java
+++ src/test/java/com/greenfox/malachit/service/ThumbnailServiceTestCreateThumbnail.java
@@ -1,6 +1,7 @@
 package com.greenfox.malachit.service;
 
 import com.greenfox.malachit.model.ThumbnailAttributes;
+import com.greenfox.malachit.model.ThumbnailAttributesDTO;
 import com.greenfox.malachit.repository.ThumbnailRepository;
 import org.junit.Before;
 import org.junit.Test;
@@ -9,7 +10,6 @@ import org.mockito.Mockito;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
 
 import static org.junit.Assert.*;
 
@@ -30,6 +30,28 @@ public class ThumbnailServiceTestCreateThumbnail {
     ThumbnailAttributes thumbnailAttributes = new ThumbnailAttributes();
     thumbnailAttributes.setId(testThumbnailAttributesId);
     Mockito.when(thumbnailRepository.findOne(testThumbnailAttributesId)).thenReturn(thumbnailAttributes);
+  }
+
+  @Test
+  public void createSingleImageDataImageIdExist() throws Exception {
+    ThumbnailAttributes thumbnailAttributes = new ThumbnailAttributes();
+    thumbnailAttributes.setId(testThumbnailAttributesId);
+    thumbnailAttributes.setType("thumbnails");
+    thumbnailAttributes.setIs_main(true);
+    thumbnailAttributes.setUploaded(false);
+    thumbnailAttributes.setCreated_at("2017-07-03T20:56:53.097");
+    thumbnailAttributes.setContent_url("https://your-hostname.com/media/images/7/content");
+    Mockito.when(thumbnailRepository.exists(testThumbnailAttributesId)).thenReturn(true);
+    Mockito.when(thumbnailRepository.findFirstByIdAndHotelEquals
+            (testThumbnailAttributesId, testHotelId)).thenReturn(thumbnailAttributes);
+    assertEquals(thumbnailService.createSingleImageData(testHotelId, testThumbnailAttributesId).getType(), "thumbnails");
+    assertEquals(thumbnailService.createSingleImageData(testHotelId, testThumbnailAttributesId).getId(), 7L);
+    ThumbnailAttributesDTO testedThumbnailAttributesDTO = (ThumbnailAttributesDTO)
+        thumbnailService
+            .createSingleImageData(testHotelId, testThumbnailAttributesId)
+            .getAttributes();
+    assertTrue(testedThumbnailAttributesDTO.isIs_main());
+    assertFalse(testedThumbnailAttributesDTO.isUploaded());
   }
 
   @Test
