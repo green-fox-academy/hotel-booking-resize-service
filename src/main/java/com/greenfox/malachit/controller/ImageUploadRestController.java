65da3ffefe7615d8199787b71375d450dcc0a7f3
Zandraa Perjesi <zandraa.perjesi@gmail.com>
2017-07-18 10:27:32 +0200

    add exception handlers to the image media endpoint

--- src/main/java/com/greenfox/malachit/controller/ImageUploadRestController.java
+++ src/main/java/com/greenfox/malachit/controller/ImageUploadRestController.java
@@ -1,22 +1,44 @@
 package com.greenfox.malachit.controller;
 
+import com.greenfox.malachit.model.FileTooLargeException;
+import com.greenfox.malachit.model.ImageDimensionTooSmallException;
+import com.greenfox.malachit.model.ImageExtensionNotValidException;
 import com.greenfox.malachit.model.ImageResponse;
+import com.greenfox.malachit.service.ErrorHandlerService;
 import com.greenfox.malachit.service.ImageService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Map;
 
 @RestController
 public class ImageUploadRestController {
 
   private ImageService imageService;
+  private ErrorHandlerService errorHandlerService;
 
   @Autowired
-  public ImageUploadRestController(ImageService imageService) {
+  public ImageUploadRestController(ImageService imageService, ErrorHandlerService errorHandlerService) {
     this.imageService = imageService;
+    this.errorHandlerService = errorHandlerService;
+  }
+
+  @ExceptionHandler(FileTooLargeException.class)
+  @ResponseStatus(HttpStatus.PAYLOAD_TOO_LARGE)
+  public Map<String, Object> handleFileTooLarge(FileTooLargeException e) {
+    return errorHandlerService.getResponse(e, "413", "The image cannot be bigger than 1MB");
+  }
+
+  @ExceptionHandler(ImageDimensionTooSmallException.class)
+  public Map<String, Object> handleImageDimensionTooSmall(ImageDimensionTooSmallException e) {
+    return errorHandlerService.getResponse(e, "413", "Not Acceptable");
+  }
+
+  @ExceptionHandler(ImageExtensionNotValidException.class)
+  public Map<String, Object> handleExtensionNotSupported(ImageExtensionNotValidException e) {
+    return errorHandlerService.getResponse(e, "406", "Not Acceptable");
   }
 
   @PostMapping(value="/media/images/{id}", headers="content-type=multipart/*")
